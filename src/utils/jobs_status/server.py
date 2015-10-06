2e7207c8b465c0a537735c5cfd3a8907eb18bd6e
Janez Krek <janez.krek@lecad.fs.uni-lj.si>
2015-10-06 18:03:16 +0200

    Updated server script for collecting status for jobs. Python script that uses threading to be more responsive. Internally it created a dict with all received statuses.

--- src/utils/jobs_status/server.py
+++ src/utils/jobs_status/server.py
@@ -2,10 +2,16 @@
 
 import socket
 import sys
+import threading
 
-class Receiver():
-    def __init__(self,inIPaddress, inPort):
-        receive = True;
+
+class SIGStatusServer():
+    # wait for clients or not
+    retrieve = True
+    # dict for storing client's status
+    clientsStatus = {}
+    
+    def __init__(self, inIPaddress, inPort):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         # Bind socket to local host and port
@@ -13,25 +19,55 @@ class Receiver():
             sock.bind((inIPaddress, inPort))
             
         except socket.error: #, msg:
-            print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+            print('Bind to '+ inIPaddress +':'+ str(inPort) +'failed.')
             sys.exit()
 
-        print('Waiting on ', inIPaddress, ':', inPort)
+        #print('Waiting on ', inIPaddress, ':', inPort)
             
-        while receive:
+        while self.retrieve:
+            # run until 
             data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-            print( "received message: ", data.decode('utf-8'), " from ", addr)
+            t1 = threading.Thread(target=SIGStatusServer.SIGSaveStatus, args=(SIGStatusServer, data, addr))
+            t1.start()
+            #print( "num active threads:", threading.active_count() )
+            #print("1:", self.clientsStatus)
 
-            if data == 'quit':
-                receive = False
+    """
+        Save status from received data/string.
+    """    
+    def SIGSaveStatus(self, inData, inIPaddr):
+
+        #print( "received message: ", inData.decode('utf-8'), " from ", inIPaddr)
+        
+        # split data into 3 parts: jobID, status, message
+        data = inData.decode('utf-8').split(";")
+        #print(data)
+        
+        if len(data) >= 1 and data[0] == 'quit':
+            # quit server
+            self.stop(self)
+
+        if len(data) == 3:
+            # save data into 
+            self.clientsStatus[data[0]] = [data[1], data[2]]
+            #print("Added status "+ data[1] + " from " + data[0] + ". Cargo: " + data[2])
+            #print("2:", self.clientsStatus)
+
+    
+    """
+        Stop waiting for clients
+    """
+    def stop(self):
+        self.retrieve = False
+        print("E:", self.clientsStatus)
+ 
  
 if __name__ == '__main__':
-
     import sys
 
 #    app = QApplication(sys.argv)
-    receiver = Receiver('127.0.0.1', 45100)
+    receiver = SIGStatusServer('127.0.0.1', 45100)
 #    receiver.show()
-    sys.exit(receiver.exec_())
+    sys.exit()
 
 
