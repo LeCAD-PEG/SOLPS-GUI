f0e32488a84b1185746e2903556873a14d757605
Leon Kos <leon.kos@lecad.fs.uni-lj.si>
2015-10-26 17:26:25 +0100

    Upgrade PyQt to 5.5.1 and SIP to 4.17

--- build-pyqt.sh
+++ build-pyqt.sh
@@ -5,9 +5,9 @@ PYTHON_VERSION=3.5.0
 PYTHON_MAINVERSION=${PYTHON_VERSION%.*}
 CMAKE_VERSION=3.3.0
 CMAKE_MAINVERSION=${CMAKE_VERSION%.*}
-PyQT_VERSION=5.5.1-snapshot-13f9ece29d02
+PyQT_VERSION=5.5.1
 QT_VERSION=5.5.1
-SIP_VERSION=4.16.9
+SIP_VERSION=4.17
 USE_QT_XCB=${USE_QT_XCB:-YES} # Use -qt-xcb for all except RHEL5 if possible
 BUILD_XCB=${BUILD_XCB:-NO} # YES if having problems with -qt-xcb
 
@@ -189,8 +189,8 @@ fi
 PYTHON=${STAGING_DIR}/bin/python${PYTHON_MAINVERSION}
 
 PyQT_SRC="PyQt-gpl-${PyQT_VERSION}.tar.gz"
-#PyQT_DOWNLOAD="http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-${PyQT_VERSION}/${PyQT_SRC}/download"
-PyQT_DOWNLOAD="https://www.riverbankcomputing.com/static/Downloads/PyQt5/${PyQT_SRC}"
+PyQT_DOWNLOAD="http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-${PyQT_VERSION}/${PyQT_SRC}/download"
+#PyQT_DOWNLOAD="https://www.riverbankcomputing.com/static/Downloads/PyQt5/${PyQT_SRC}"
 
 if [ ! -f ${DOWNLOAD_DIR}/${PyQT_SRC} ]; then 
     wget  -O ${DOWNLOAD_DIR}/${PyQT_SRC} ${PyQT_DOWNLOAD}
