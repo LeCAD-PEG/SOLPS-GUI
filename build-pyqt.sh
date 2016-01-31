0de8be98f28ef3e73d5ba200c5d0292d58ddad52
Leon Kos <leon.kos@lecad.fs.uni-lj.si>
2016-01-31 10:58:32 +0100

    Building on IPP Gateway now works

--- build-pyqt.sh
+++ build-pyqt.sh
@@ -1,28 +1,37 @@
 #!/bin/sh -x
-
-MAKE_JOBS=${MAKE_JOBS:-4}
+## Building PyQt with Python3 and Qt5
+ 
 PYTHON_VERSION=3.5.1
 PYTHON_MAINVERSION=${PYTHON_VERSION%.*}
-CMAKE_VERSION=3.4.1
-CMAKE_MAINVERSION=${CMAKE_VERSION%.*}
-PyQT_VERSION=5.5.1
 QT_VERSION=5.5.1
+PyQT_VERSION=5.5.1 # should be the same as Qt 
 SIP_VERSION=4.17
-USE_QT_XCB=${USE_QT_XCB:-YES} # Use -qt-xcb for all except RHEL5 if possible
-BUILD_XCB=${BUILD_XCB:-NO} # YES if having problems with -qt-xcb
 
+# Site specific defaults
 case $(hostname) in
-  *.iter.org) 
+  *.iter.org) # RHEL5.11 with GCC 4.1
 	module purge
-	BUILD_XCB="YES"
 	USE_QT_XCB="NO"
-	unset CXX CC # we don't want ICC 11.1 to be selected by chance
+	BUILD_XCB="YES"
+	unset CXX CC # Remove ICC to be selected by chance
+        QT_EXTRA_FLAGS=${QT_EXTRA_FLAGS:-\
+                        -D GLX_GLXEXT_LEGACY \
+                        -D _X_INLINE=inline \
+                        -D FC_WEIGHT_EXTRABLACK=215 \
+                        -D FC_WEIGHT_ULTRABLACK=FC_WEIGHT_EXTRABLACK}
 	;;
-  *)
-	MAKE_JOBS=${MAKE_JOBS:-8}
+  g0[1234]*) # SLES 11.4 WPCD Gateway (incompatible XCB, Xlib and GL libraries)
+        MAKE_JOBS=${MAKE_JOBS:-8}
+	USE_QT_XCB="NO"
+	BUILD_XCB="YES"
+        QT_EXTRA_FLAGS=${QT_EXTRA_FLAGS:--no-sql-mysql \
+                        -skip qtcanvas3d -skip qtquick1}
 	;;
 esac
 
+MAKE_JOBS=${MAKE_JOBS:-4} # safe default nowadays
+USE_QT_XCB=${USE_QT_XCB:-NO} # Use -qt-xcb for all except RHEL5 if possible
+BUILD_XCB=${BUILD_XCB:-NO} # YES if having problems with -qt-xcb
 
 BUILDROOT=${PWD}
 BUILD_DIR=${BUILDROOT}/build
@@ -33,19 +42,17 @@ STAGING_QT=${STAGING_DIR}/qt/${QT_VERSION}
 
 set -e
 
-#Initialize directories
+## Initialize directories
 
 install -d ${BUILD_DIR}
 install -d ${STAGING_DIR}
 install -d ${DOWNLOAD_DIR}
 
-LD_LIBRARY_PATH="${STAGING_DIR}/lib:${LD_LIBRARY_PATH}"
-export LD_LIBRARY_PATH
-
-## Install python
+## Install Python3
 
 PYTHON_SRC="Python-${PYTHON_VERSION}.tgz"
-PYTHON_DOWNLOAD="https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz"
+PYTHON_SITE="https://www.python.org/ftp/python"
+PYTHON_DOWNLOAD="${PYTHON_SITE}/${PYTHON_VERSION}/${PYTHON_SRC}"
 
 if [ ! -f ${DOWNLOAD_DIR}/${PYTHON_SRC} ]; then 
     wget  -O ${DOWNLOAD_DIR}/${PYTHON_SRC} ${PYTHON_DOWNLOAD}
@@ -61,7 +68,8 @@ if [ ! -e   ${PYTHON_SRC_DIR}/.built ]; then
   cd ${PYTHON_SRC_DIR}
   # LDFLAGS for OpenSSL static build see the following
   # http://stackoverflow.com/questions/7307857/libssl-static-lib-compiling-issue-with-fpic
-  LDFLAGS="-Wl,-Bsymbolic" ./configure --prefix=${STAGING_DIR} --enable-shared
+  LDFLAGS="-Wl,-Bsymbolic,-rpath=${STAGING_DIR}/lib" \
+  ./configure --prefix=${STAGING_DIR} --enable-shared
   LD_PRELOAD=/usr/lib64/libgssapi_krb5.so:/usr/lib64/libz.so \
   make -j ${MAKE_JOBS}
   LD_PRELOAD=/usr/lib64/libgssapi_krb5.so:/usr/lib64/libz.so \
@@ -76,17 +84,18 @@ if [ "${USE_QT_XCB}" = "YES" ]; then # build QT with QT-prvided XCB libs
   XCB_FLAGS="${XCB_FLAGS} -qt-xcb"
 fi
 
-export PYTHON=${STAGING_DIR}/bin/python3
-## Build XCB and libXML for Qt5 locally instead of Qt provided XCB libs. 
+PYTHON="${STAGING_DIR}/bin/python${PYTHON_MAINVERSION}"
+
+## Build XCB Xlib and libXML for Qt5 locally instead of Qt provided XCB libs.
 # For Qt5.x build problems on RHEL5 see
 # https://forum.qt.io/topic/37757/howto-building-qt-5-2-1-including-webkit-on-rhel5-linux-centos-5-7
-#See http://kate-editor.org/2014/12/22/qt-5-4-on-red-hat-enterprise-5/
+# See http://kate-editor.org/2014/12/22/qt-5-4-on-red-hat-enterprise-5/
 if [ "${BUILD_XCB}" = "YES" ]
     then
     install -d  ${BUILD_DIR}/xcb
     cd ${BUILD_DIR}/xcb
     for url in \
-http://xmlsoft.org/sources/libxml2-2.9.2.tar.gz \
+http://xmlsoft.org/sources/libxml2-2.9.3.tar.gz \
 http://xorg.freedesktop.org/archive/individual/proto/xproto-7.0.28.tar.gz \
 http://xcb.freedesktop.org/dist/xcb-proto-1.11.tar.gz \
 http://xcb.freedesktop.org/dist/libpthread-stubs-0.3.tar.gz \
@@ -97,6 +106,7 @@ http://xcb.freedesktop.org/dist/xcb-util-keysyms-0.4.0.tar.gz \
 http://xcb.freedesktop.org/dist/xcb-util-wm-0.4.1.tar.gz \
 http://xcb.freedesktop.org/dist/xcb-util-renderutil-0.3.9.tar.gz \
 http://xcb.freedesktop.org/dist/xcb-util-cursor-0.1.2.tar.gz \
+http://www.x.org/releases/X11R7.7/src/lib/libX11-1.5.0.tar.gz \
    ; do
       file=${url##*/}
       test -f ${file} || wget ${url}
@@ -108,9 +118,9 @@ http://xcb.freedesktop.org/dist/xcb-util-cursor-0.1.2.tar.gz \
       if [ "${pkgdir%%-*}" = "libxml2" ]; then configopt="--without-python"
       else configopt=
       fi
-      PKG_CONFIG_PATH=${STAGING_DIR}/lib/pkgconfig \
-	  ./configure --prefix=${STAGING_DIR} ${configopt} 
-      make
+      PKG_CONFIG_PATH=${STAGING_DIR}/lib/pkgconfig:${PKG_CONFIG_PATH} \
+      PYTHON=${PYTHON} ./configure --prefix=${STAGING_DIR} ${configopt}
+      make -j ${MAKE_JOBS}
       make install
       touch .built
       cd ..
@@ -122,25 +132,26 @@ http://xcb.freedesktop.org/dist/xcb-util-cursor-0.1.2.tar.gz \
 fi
 
 
-#Install QT
+## Install QT
 
 QT_MAJOR_VERSION=${QT_VERSION%.*}
 QT_TAR="qt-everywhere-opensource-src-${QT_VERSION}.tar.gz"
-QT_DOWNLOAD="http://download.qt.io/official_releases/qt/${QT_MAJOR_VERSION}/${QT_VERSION}/single/${QT_TAR}"
-#QT_DOWNLOAD="http://download.qt.io/development_releases/qt/${QT_MAJOR_VERSION}/${QT_VERSION}/single/${QT_TAR}"
+QT_SITE="http://download.qt.io/official_releases/qt"
+#QT_SITE="http://download.qt.io/development_releases/qt/"
+QT_DOWNLOAD="${QT_SITE}/${QT_MAJOR_VERSION}/${QT_VERSION}/single/${QT_TAR}"
+
 QT_SOURCE_DIR="${BUILD_DIR}/qt-everywhere-opensource-src-${QT_VERSION}"
 
-#Download tar and unpack
 if [ ! -f ${DOWNLOAD_DIR}/${QT_TAR} ]; then
   wget -P ${DOWNLOAD_DIR} ${QT_DOWNLOAD}
 fi
 
-if [ ! -e ${QT_SOURCE_DIR}/.built ]; then
-  #Building QT
+export LD_LIBRARY_PATH="${STAGING_DIR}/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+
+if [ ! -e ${QT_SOURCE_DIR}/.configured ]; then # Configuring Qt
   rm -rf ${QT_SOURCE_DIR} ${STAGING_QT} 
   cd ${BUILD_DIR}
   tar xzf ${DOWNLOAD_DIR}/${QT_TAR} 
-  
   cd ${QT_SOURCE_DIR}
   sed -i.orig -e 's/-Wno-error=return-type//' \
       qtlocation/src/3rdparty/poly2tri/poly2tri.pro
@@ -149,28 +160,34 @@ if [ ! -e ${QT_SOURCE_DIR}/.built ]; then
   patch -p 1 -d ${QT_SOURCE_DIR} < ${PATCH_DIR}/qt5-qfbvthandler.patch
   patch -p 1 -d ${QT_SOURCE_DIR} < ${PATCH_DIR}/qglxintegration-glx-context.patch
   patch -p 1 -d ${QT_SOURCE_DIR} < ${PATCH_DIR}/qt5-qxcbconnection.patch
-
-  PKG_CONFIG_PATH=${STAGING_DIR}/lib/pkgconfig \
+  sed -i -e '/auto/d' qtdeclarative/tests/tests.pro \
+                      qtmultimedia/tests/tests.pro \
+                      qtgraphicaleffects/tests/tests.pro
+  PKG_CONFIG_PATH=${STAGING_DIR}/lib/pkgconfig:${PKG_CONFIG_PATH} \
     ./configure -v --prefix=${STAGING_QT} -opensource -confirm-license \
       -shared -no-audio-backend -skip qtwebkit -skip qtwebkit-examples \
-      -skip qt3d ${XCB_FLAGS} \
-      -qt-xkbcommon -xkb-config-root /usr/share/X11/xkb \
-      -D GLX_GLXEXT_LEGACY \
-      -D _X_INLINE=inline \
-      -D FC_WEIGHT_EXTRABLACK=215 \
-      -D FC_WEIGHT_ULTRABLACK=FC_WEIGHT_EXTRABLACK
-  make -j ${MAKE_JOBS}
-  make install
-  PATH="${STAGING_QT}/bin:${PATH}" make qmake_all docs install_docs 
-  touch ${QT_SOURCE_DIR}/.built
+      -skip qt3d ${XCB_FLAGS} ${QT_EXTRA_FLAGS} \
+      -qt-xkbcommon -xkb-config-root /usr/share/X11/xkb
+  touch ${QT_SOURCE_DIR}/.configured
 fi
 
-#Install sip
+if [ ! -e ${QT_SOURCE_DIR}/.built ]; then  ## Building Qt and docs
+  cd ${QT_SOURCE_DIR}
+  make -j ${MAKE_JOBS}
+  make install
+  # Building Qt documentation
+  PATH="${STAGING_QT}/bin:${PATH}" make -C qtbase/src sub-qdoc
+  PATH="${STAGING_QT}/bin:${PATH}" make -C qttools
+  PATH="${STAGING_QT}/bin:${PATH}" make qmake_all
+  PATH="${STAGING_QT}/bin:${PATH}" make -j ${MAKE_JOBS} docs install_docs
+  touch ${QT_SOURCE_DIR}/.built
+fi # building Qt
 
-PYTHON=${STAGING_DIR}/bin/python${PYTHON_MAINVERSION}
+## Install sip
 
 SIP_SRC="sip-${SIP_VERSION}.tar.gz"
-SIP_DOWNLOAD="http://sourceforge.net/projects/pyqt/files/sip/sip-${SIP_VERSION}/${SIP_SRC}"
+SIP_SITE="http://sourceforge.net/projects/pyqt/files/sip"
+SIP_DOWNLOAD="${SIP_SITE}/sip-${SIP_VERSION}/${SIP_SRC}"
 
 if [ ! -f ${DOWNLOAD_DIR}/${SIP_SRC} ]; then 
     wget  -O ${DOWNLOAD_DIR}/${SIP_SRC} ${SIP_DOWNLOAD}
@@ -184,19 +201,19 @@ if [ ! -e   ${SIP_SRC_DIR}/.built ]; then
   cd ${BUILD_DIR}
   tar xzf ${DOWNLOAD_DIR}/${SIP_SRC}
   cd ${SIP_SRC_DIR}
-  ${PYTHON} configure.py 
+  ${PYTHON} configure.py
   make -j ${MAKE_JOBS}
   make install 
   touch ${SIP_SRC_DIR}/.built
 fi
 
-#Install PyQT
-
-PYTHON=${STAGING_DIR}/bin/python${PYTHON_MAINVERSION}
+## Install PyQT
 
 PyQT_SRC="PyQt-gpl-${PyQT_VERSION}.tar.gz"
-PyQT_DOWNLOAD="http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-${PyQT_VERSION}/${PyQT_SRC}/download"
-#PyQT_DOWNLOAD="https://www.riverbankcomputing.com/static/Downloads/PyQt5/${PyQT_SRC}"
+PyQT_SITE="http://sourceforge.net/projects/pyqt/files/PyQt5"
+#PyQT_SITE="https://www.riverbankcomputing.com/static/Downloads/PyQt5}"
+PyQT_DOWNLOAD="${PyQT_SITE}/PyQt-${PyQT_VERSION}/${PyQT_SRC}/download"
+
 
 if [ ! -f ${DOWNLOAD_DIR}/${PyQT_SRC} ]; then 
     wget  -O ${DOWNLOAD_DIR}/${PyQT_SRC} ${PyQT_DOWNLOAD}
@@ -217,4 +234,3 @@ if [ ! -e   ${PyQT_SRC_DIR}/.built ]; then
   make install 
   touch ${PyQT_SRC_DIR}/.built
 fi
-
