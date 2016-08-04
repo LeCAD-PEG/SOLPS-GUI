55c065a63737a73225b07ca2a928e3d383bbbfa1
Leon Kos <leon.kos@lecad.fs.uni-lj.si>
2016-08-04 10:47:19 +0200

    Gateway build fixes for ParaView

--- build-paraview-plugin.sh
+++ build-paraview-plugin.sh
@@ -1,11 +1,9 @@
 #!/bin/sh -x
 
-
 MAKE_JOBS=${MAKE_JOBS:-4}
-
 BUILDROOT="${PWD}"
 BUILD_DIR="${BUILDROOT}/build"
-PARAVIEW_VERSION="5.0.0"
+PARAVIEW_VERSION="5.1.0"
 STAGING_DIR="${BUILDROOT}/staging"
 STAGING_PARAVIEW="${STAGING_DIR}/paraview/${PARAVIEW_VERSION}"
 STAGING_PLUGINS="${STAGING_DIR}/paraview-plugins/${PARAVIEW_VERSION}/${DATAVERSION}"
@@ -31,7 +29,8 @@ name=Edge
 install -d ${BUILD_DIR}/Plugins-ReadUAL${name}
 install -d ${STAGING_PLUGINS}
 cd ${BUILD_DIR}/Plugins-ReadUAL${name}
-cmake -DCMAKE_BUILD_TYPE:STRING=Debug \
+PATH=${STAGING_DIR}/bin:${PATH} \
+ cmake -DCMAKE_BUILD_TYPE:STRING=Debug \
     -DCMAKE_INSTALL_PREFIX:PATH=${STAGING_PARAVIEW} \
     -DParaView_DIR:PATH=${STAGING_PARAVIEW} \
     ${BUILDROOT}/src/plugins/paraview 
