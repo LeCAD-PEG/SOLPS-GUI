e3110c8db74d4baadb3ae391e19d73cb5d06b025
Gregor Simič <gregor.simic@lecad.fss.uni-lj.si>
2017-08-29 10:46:47 +0200

    Plugins added for getIDS and putIDS.

---
+++ src/plugins/designer/getIDSplugin.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+"""
+
+A SOLPS getIDS plugin for Qt designer.
+
+"""
+
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtDesigner import QPyDesignerCustomWidgetPlugin
+
+from get_edge_ids import getIDS
+
+class getIDSplugin(QPyDesignerCustomWidgetPlugin):
+    """Plugin for put_edge_ids functionality.
+    """
+    def __init__(self, parent=None):
+        super(getIDSplugin, self).__init__(parent)
+
+    def createWidget(self, parent):
+        return getIDS(parent)
+
+    def name(self):
+        return "getIDS"
+
+    def group(self):
+        return "SOLPS"
+
+    def icon(self):
+        return QIcon(_logo_pixmap)
+
+    def toolTip(self):
+        return "Push button for putting data to IDS data entry."
+
+    def whatsThis(self):
+        return ""
+
+    def isContainer(self):
+        return False
+
+    def domXml(self):
+        return '<widget class="getIDS" name="get_edge_ids">\n</widget>'
+
+    def includeFile(self):
+        return "get_edge_ids"
+
+# Define the image used for the icon.
+_logo_16x16_xpm = [
+    "16 16 12 1",
+    "  c gray100",
+    ". c #FF3333",
+    "X c #CCFFFF",
+    "o c #99CCFF",
+    "O c #66CCFF",
+    "+ c #CCCCFF",
+    "@ c #CCFF33",
+    "# c #99FFFF",
+    "$ c #CCFFCC",
+    "% c #99CCCC",
+    "& c #FFCC66",
+    "* c #66CCCC",
+    "   XX      . .  ",
+    "  OOoOX     . . ",
+    " Xo   oX        ",
+    " O     o    .   ",
+    " o      X  . .  ",
+    " o      X  .... ",
+    " o      X       ",
+    " o      X     . ",
+    " o      X  .... ",
+    " o     X        ",
+    " X+    X    ..  ",
+    "  O   o    .  . ",
+    "  O++oX     ..  ",
+    "  #*o%          ",
+    "  $@&      . .  ",
+    "            . . "]
+
+_logo_pixmap = QPixmap(_logo_16x16_xpm)
