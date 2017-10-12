a4d8efef948a68de2ef588eed299b02addf29dbd
Simon Pinches <Simon.Pinches@iter.org>
2017-10-12 15:31:35 +0200

    Changes to build/install paraview-plugins at ITER

--- build-paraview-plugin.sh
+++ build-paraview-plugin.sh
@@ -13,7 +13,7 @@ STAGING_PLUGINS="${STAGING_DIR}/paraview-plugins/${PARAVIEW_VERSION}/${IMAS_VERS
 case $(hostname -f) in
   *.iter.org) 
 	module purge
-        module load imas/3.10.1/ual/3.6.0 blitz/0.10 binutils/2.25
+        module load imas
         module load OpenSSL/1.0.2g-GCC-4.8.3
         module load Python/2.7.9-goolf-1.5.16 # overwrite Anaconda
 	export CC=gcc
