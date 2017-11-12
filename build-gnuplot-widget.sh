9a31d4106e9b38494cc7d02ca82443d7432b6924
Leon Kos <leon.kos@lecad.fs.uni-lj.si>
2017-11-12 19:05:11 +0100

    Gnuplot Widget build cleanup

--- build-gnuplot-widget.sh
+++ build-gnuplot-widget.sh
@@ -1,27 +1,26 @@
 #!/bin/sh -x
 
+QT_VERSION=${QT_VERSION:-5.9.1}
+PYTHON_VERSION=${PYTHON_VERSION:-3.6.1}
+
 BUILDROOT=${PWD}
-GNUPLOT_WIDGET_DIR=${GNUPLOT_WIDGET_DIR:-$BUILDROOT/src/gnuplot-widget}
-SOURCES_DIR=$GNUPLOT_WIDGET_DIR/src
-STAGING_DIR=${STAGING_DIR:-${BUILDROOT}/staging/lib/site-packages}
-QT_VERSION="5.9.1"
-QTDIR="${BUILDROOT}/staging/qt/$QT_VERSION"
-# SOurce files.
-FILES="$SOURCES_DIR/Qt*.h $SOURCES_DIR/gnuplotWidget.h"
+STAGING_DIR=${STAGING_DIR:-${BUILDROOT}/staging}
+QTDIR=${STAGING_DIR}/qt/${QT_VERSION}
+PYVER=${PYTHON_VERSION%.*} # just major.minor version 
+INSTALL_DIR=${INSTALL_DIR:-${STAGING_DIR}/lib/python${PYVER}/site-packages}
+GNUPLOT_WIDGET_DIR=${GNUPLOT_WIDGET_DIR:-${BUILDROOT}/src/gnuplot-widget}
+GNUPLOT_WIDGET_SRC_DIR=${GNUPLOT_WIDGET_DIR}/src
+
 
 # Where to store generated sip file
-SIPDIR=${SIPDIR:-$BUILDROOT/staging/share/sip/PyQt5}
+SIPDIR=${SIPDIR:-${STAGING_DIR}/share/sip/PyQt5}
 
 # Where to store compiled objects
-OBJECTS_DIR=${OBJECTS_DIR:-$BUILDROOT/build/gnuplotWidget}
+OBJECTS_DIR=${OBJECTS_DIR:-${BUILDROOT}/build/gnuplotWidget}
 
-QT_LIBS="${QTDIR}/lib"
-export QT_LIBS
-export PATH="${BUILDROOT}/staging/bin:${QTDIR}/bin:${PATH}"
-export LD_LIBRARY_PATH="${BUILDROOT}/staging/lib:${QTDIR}/lib:${LD_LIBRARY_PATH}"
-export LD_LIBRARY_PATH="${BUILDROOT}/staging/qt/$QT_VERSION/lib:${LD_LIBRARY_PATH}"
-export PKG_CONFIG_PATH="${BUILDROOT}/staging/lib/pkgconfig:${PKG_CONFIG_PATH}"
-
+export PATH="${STAGING_DIR}/bin:${QTDIR}/bin:${PATH}"
+export LD_LIBRARY_PATH="${STAGING_DIR}/lib:${QTDIR}/lib:${LD_LIBRARY_PATH}"
+export PKG_CONFIG_PATH="${STAGING_DIR}/lib/pkgconfig:${PKG_CONFIG_PATH}"
 
 case $(hostname -f) in
     *.iter.org)
@@ -33,57 +32,37 @@ case $(hostname -f) in
         ;;
 esac
 
-# Old ITER CASE
-# case $(hostname -f) in
-#   *.iter.org)
-#         module purge
-#     #module load GCC
-#         module load imas binutils
-#         module unload Anaconda2
-#         qmake --version && sip -V
-#         QT_VERSION=5.6.2 PyQT_VERSION=5.6.2 SIP_VERSION=4.18
-#         QTDIR="${EBROOTANACONDA3}/pkgs/qt-5.6.2-3"
-#         MAKE_JOBS=${MAKE_JOBS:-4}
-#         # Because qmake cannot find mkspecs file on its own
-#         export QMAKESPEC=$QTDIR/mkspecs/linux-g++
-#     ;;
-#   *)
-#     ;;
-# esac
-
+MAKE_JOBS=${MAKE_JOBS:-4}
 export QTDIR
 
 set -e
 
-cd $GNUPLOT_WIDGET_DIR
+cd ${GNUPLOT_WIDGET_DIR}
 
-# mocing .h files
-for file in $FILES
-do
-    moc $file -o $SOURCES_DIR/moc_$(basename $file .h).cpp
+which moc
+which qmake
+
+SOURCE_FILES="${GNUPLOT_WIDGET_SRC_DIR}/Qt*.h ${GNUPLOT_WIDGET_SRC_DIR}/gnuplotWidget.h"
+for file in ${SOURCE_FILES} # MOCing .h files
+  do moc ${file} -o ${GNUPLOT_WIDGET_SRC_DIR}/moc_$(basename ${file} .h).cpp
 done
 
-if [ ! -d "$SIPDIR" ]; then
-  mkdir $SIPDIR
-fi
-
-if [ ! -d "$OBJECTS_DIR" ]; then
-  echo "no objects directory, creating it"
-  mkdir $OBJECTS_DIR
-fi
-python3 configure.py --verbose --sipdir="$SIPDIR" --outdir="$OBJECTS_DIR" --srcdir="$SOURCES_DIR" --incdir="$SOURCES_DIR" --destdir="$STAGING_DIR"
+install -d ${SIPDIR}
+install -d ${OBJECTS_DIR}
 
 
+python3 configure.py --verbose --sipdir=${SIPDIR} --outdir=${OBJECTS_DIR} \
+    --srcdir=${GNUPLOT_WIDGET_SRC_DIR} --incdir=${GNUPLOT_WIDGET_SRC_DIR} \
+    --destdir=${INSTALL_DIR}
 
 # TODO: Move all compiling and other generated files to build dir.
 # TODO: Safely remove as to not accidentally remove non-generated files.
 
-cd $SOURCES_DIR
+cd ${GNUPLOT_WIDGET_SRC_DIR}
 
-make -j $MAKE_JOBS
+make -j ${MAKE_JOBS}
 make install
 make clean
 make distclean
-echo "Cleaning up"
-rm moc_* sippy* sipAPI* pyQtGnuplot.*
-rm -r $OBJECTS_DIR
+rm -f moc_* sippy* sipAPI* pyQtGnuplot.*
+rm -fr ${OBJECTS_DIR}
