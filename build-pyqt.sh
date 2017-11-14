11838d14f1d6d559fcc6ce83ed0c5664499ed86a
Leon Kos <leon.kos@lecad.fs.uni-lj.si>
2017-11-14 17:57:12 +0100

    Allow PYTHON_VERSION to be specified before building PyQt

--- build-pyqt.sh
+++ build-pyqt.sh
@@ -2,7 +2,7 @@
 ## Building PyQt with Python3 and Qt5
 ## Minimum GCC supported version for building Qt5 is 4.7
 
-PYTHON_VERSION=3.6.3
+PYTHON_VERSION=${PYTHON_VERSION:-3.6.3}
 PYTHON_MAINVERSION=${PYTHON_VERSION%.*}
 QT_VERSION=5.9.1
 PyQT_VERSION=5.9.1 # should be the same as Qt
