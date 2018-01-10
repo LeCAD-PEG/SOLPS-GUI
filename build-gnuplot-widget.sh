f28f1ff66548fb7dfd472fa2de9b35456a800091
Leon Kos <leon.kos@lecad.fs.uni-lj.si>
2018-01-10 20:51:31 +0100

    Absolute path from BUILDROOT

--- build-gnuplot-widget.sh
+++ build-gnuplot-widget.sh
@@ -3,7 +3,7 @@
 QT_VERSION=${QT_VERSION:-5.9.1}
 PYTHON_VERSION=${PYTHON_VERSION:-3.6.4}
 
-BUILDROOT=${PWD}
+BUILDROOT=$(cd ${0%/*} && echo ${PWD})
 STAGING_DIR=${STAGING_DIR:-${BUILDROOT}/staging}
 QTDIR=${STAGING_DIR}/qt/${QT_VERSION}
 
@@ -17,7 +17,7 @@ OBJECTS_DIR=${OBJECTS_DIR:-${BUILDROOT}/build/gnuplotWidget}
 case $(hostname -f) in
     *.iter.org)
         module purge
-        module load GCC
+        module load GCC/4.8.3 binutils/2.25
         MAKE_JOBS=${MAKE_JOBS:-4}
         ;;
    *.marconi.cineca.it)
