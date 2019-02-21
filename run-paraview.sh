6bc3d6ffd27e427885f33bce1455a1c47f3c52a3
Gregor Simič <gregor.simic@lecad.fs.uni-lj.si>
2019-02-21 09:09:56 +0100

    For running ParaView locally with ReadUAL-Edge plugin, IMAS_VERSION and UAL_VERSION environment variables MUST be set and exported, otherwise an exception occurs in ParaView, leading to crash.

--- run-paraview.sh
+++ run-paraview.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 PARAVIEW_VERSION=${PARAVIEW_VERSION:-5.4.1}
+PARAVIEW_MAINVERSION=${PARAVIEW_VERSION%.*}
+
 QT_VERSION=${QT_VERSION:-4.8.7}
+BUILDROOT=${PWD}
+BUILD_DIR=${BUILDROOT}/build
+DOWNLOAD_DIR=${DOWNLOAD_DIR:-${BUILDROOT}/download}
+STAGING_DIR=${STAGING_DIR:-${BUILDROOT}/staging}
+
+
+
 
 case $(hostname -f) in
-  *.iter.org) 
+  *.iter.org)
 	module purge
 	# The following modules are needed for IMAS plugins
 	module load IMAS/3.21.0-3.8.6 binutils/2.28-GCCcore-6.4.0 Blitz++/0.10-GCCcore-6.4.0
@@ -18,23 +27,26 @@ case $(hostname -f) in
 	module unload matlab
 	module switch itm-python/2.7
 	imasdb solps-iter
-	imasdb	
+	imasdb
 	;;
-  
+
   *)
+	export IMAS_VERSION=3.21.0
+	export UAL_VERSION=3.8.4
+	MDSPLUS_VERSION=stable_release-7-7-8
+	export LD_LIBRARY_PATH=${STAGING_DIR}/qt/${QT_VERSION}/lib:${LD_LIBRARY_PATH}
+	export LD_LIBRARY_PATH=${STAGING_DIR}/mdsplus/${MDSPLUS_VERSION}/lib:${LD_LIBRARY_PATH}
+	export LD_LIBRARY_PATH=${STAGING_DIR}/access-layer/${UAL_VERSION}/lib:${LD_LIBRARY_PATH}
+	export LD_LIBRARY_PATH=${STAGING_DIR}/lib:${LD_LIBRARY_PATH}
+	export LD_LIBRARY_PATH=${STAGING_DIR}/paraview/${PARAVIEW_VERSION}/lib/paraview-${PARAVIEW_MAINVERSION}:${LD_LIBRARY_PATH}
+	export ids_path=${STAGING_DIR}/access-layer/${UAL_VERSION}/models/mdsplus
 	;;
 esac
 
-BUILDROOT=${PWD}
-BUILD_DIR=${BUILDROOT}/build
-DOWNLOAD_DIR=${DOWNLOAD_DIR:-${BUILDROOT}/download}
-STAGING_DIR=${STAGING_DIR:-${BUILDROOT}/staging}
 #STAGING_DIR=/work/imas/project
-STAGING_QT=${STAGING_QT:-${STAGING_DIR}/qt/${QT_VERSION}}
 STAGING_PARAVIEW=${STAGING_PARAVIEW:-$STAGING_DIR/paraview/$PARAVIEW_VERSION}
 
 #install -d /tmp/${USER}
 
-LD_LIBRARY_PATH=${STAGING_QT}/lib:${LD_LIBRARY_PATH} \
 ${STAGING_PARAVIEW}/bin/paraview $@
 
